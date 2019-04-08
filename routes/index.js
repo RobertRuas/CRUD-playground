a7d37531ca3ee47a0611eee0c396d55f55e04a03
Robert Ruas <92ruas@gmail.com>
2019-04-09 00:14:39 +0100

    Telas de usuario
    Telas de usuario

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { page: 'home' });
+});
+
+/* GET Users pages. */
+router.get('/users', function(req, res, next) {
+  res.render('users/list', { page: 'users-list' });
+});
+
+router.get('/users/add', function(req, res, next) {
+  res.render('users/add', { page: 'users-add' });
+});
+
+router.get('/users/edit', function(req, res, next) {
+  res.render('users/edit', { page: 'users-edit' });
 });
 
 module.exports = router;
