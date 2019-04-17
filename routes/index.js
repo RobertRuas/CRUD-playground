6bf0e6d0e467f7143e5ee2ea0f52c20530ceba49
Robert Ruas <92ruas@gmail.com>
2019-04-17 11:32:56 +0100

    testes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET test page. */
+router.get('/teste', function(req, res, next) {
+  res.render('teste', { page: 'teste' });
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { page: 'home' });
@@ -15,8 +20,16 @@ router.get('/users/add', function(req, res, next) {
   res.render('users/add', { page: 'users-add' });
 });
 
-router.get('/users/edit', function(req, res, next) {
-  res.render('users/edit', { page: 'users-edit' });
+router.get('/users/edit/:tagId', function(req, res, next) {
+  res.render('users/edit', { page: 'users-edit', tagId: req.params.tagId });
 });
+
+router.get('/users/drop/:tagId', function(req, res, next) {
+  res.send("Drop user with ID " + req.params.tagId);
+});
+
+// Exeption tratative URLs
+router.get('/users/edit', function(req, res, next) { res.redirect('/users'); });
+router.get('/users/drop', function(req, res, next) { res.redirect('/users'); });
 
 module.exports = router;
